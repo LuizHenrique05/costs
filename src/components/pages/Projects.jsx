27f9b6af5bebe06972dc149ee956c1d39522843d
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2021-09-22 22:02:05 -0300

    second commit

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,4 +1,32 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import Message from '../layout/Message'
 
-export default props => 
-    <h1>Projects</h1>
+import styles from './Projects.module.css'
+
+export default props => {
+
+    const location = useLocation()
+    let message = ''
+    if(location.state)
+        message = location.state.message
+
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Projects</h1>
+                <LinkButton to="/newproject" text="New project" />
+            </div>
+            
+            {message && (
+                <Message msg={message} type="success" />
+            )}
+            <Container customClass="start">
+                <p>projects</p>
+            </Container>
+        </div>
+    )
+}
+    
