3d1deab281d54d8551d452495e719db09dad0ad3
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2021-10-13 22:58:10 -0300

    edit project

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router' 
+import { useParams } from 'react-router'
+import Container from '../layout/Container'
+import Loading from '../layout/Loading'
+import Message from '../layout/Message'
+import ProjectForm from '../project/ProjectForm'
 
 import styles from './Project.module.css'
 
 export default () => {
     const { id } = useParams()
     const [project, setProject] = useState([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
+    const [message, setMessage] = useState()
+    const [type, setType] = useState()
 
     useEffect(() => {
 
@@ -17,14 +24,72 @@ export default () => {
         })
         .then(res => res.json())
         .then((data) => {
-            console.log(data)
             setProject(data)
         })
         .catch(err => console.log(err))
-
     }, [id])
 
+    const editPost = (project) => {
+        if (project.budget < project.costs) {
+            setMessage('Budget cannot be less than project cost!')
+            setType('error')
+            return false
+        }
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        })
+        .then(res => res.json())
+        .then(data => {
+            setProject(data)
+            setShowProjectForm(false)
+            setMessage('project Updated!')
+            setType('success')
+        })
+        .catch(err => console.log(err))
+    }
+
+    const toggleProjectForm = () => {
+        setShowProjectForm(!showProjectForm)
+    }
+
     return (
-        <p>{project.name}</p>
+        <>
+            {project.name ? (
+                <div className={styles.project_details}>
+                    <Container customClass="column">
+                        {message && <Message type={type} msg={message} />}
+                        <div className={styles.details_container}>
+                            <h1>Project: {project.name}</h1>
+                            <button className={styles.btn} onClick={toggleProjectForm}>
+                                {!showProjectForm ? 'Edit' : 'Close'}
+                            </button>
+                            {!showProjectForm ? (
+                                <div className={styles.project_info}>
+                                    <p>
+                                        <span>Category:</span> {project.category.name}
+                                    </p>
+                                    <p>
+                                        <span>Total Budget:</span> R${project.budget}
+                                    </p>
+                                    <p>
+                                        <span>Total Utilized:</span> R${project.costs}
+                                    </p>
+                                </div>
+                            ) : (
+                                <div className={styles.project_info}>
+                                    <ProjectForm handleSubmit={editPost} btnText="Update" projectData={project} />
+                                </div>
+                            )}
+                        </div>
+                    </Container>
+                </div>
+            ) : (
+                <Loading />
+            )}
+        </>
     )
 }
